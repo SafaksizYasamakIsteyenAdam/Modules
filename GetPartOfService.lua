a8e6fe7774674ebd8ecd6a7786a7640781dca511
SafaksizYasamakIsteyenAdam <safakoperasyonuistemem@gmail.com>
2024-05-31 20:56:37 +0300

    Delete GetPartOfService.lua

--- GetPartOfService.lua
+++
@@ -1,8 +0,0 @@
-local function GetPartOfService(service)
-    local tableService = game:GetService(service)
-    local serviceChildrens = {
-        Parent = tableService.Parent,
-        ClassName = tableService.ClassName
-    }
-    return serviceChildrens
-end
